7673c538d7f5bdfa53c2021d51443e2c5f687162
sccengizlrn <sccengizlrn@gmail.com>
2025-08-01 22:14:15 +0300

    i

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>
+        Selam, ben Ali Alkım Duman. Ama insanlar bana kısaca Zeki Demirkubuz
+        derler.
+      </h1>
+    </>
+  );
+}
+
+export default App;
